9f5369a3800c502ffff3c35684ab25c79ec40621
CHRISTOPHER OKOH <etzkristokency2@gmail.com>
2026-09-14 17:15:21 +0100

    Add bar chart visualization for student scores

---
+++ active/task3.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+
+student = {
+    "name": "Christopher",
+    "scores": {
+        "Math": 80,
+        "Python": 95,
+        "Database": 75,
+        "AI": 90
+    }
+}
+
+
+subjects = list(student["scores"].keys())
+
+marks = list(student["scores"].values())
+
+
+plt.bar(subjects, marks)
+
+plt.title(f"{student['name']} Scores")
+
+plt.xlabel("Subjects")
+plt.ylabel("Marks")
+
+plt.show()
